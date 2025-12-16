c655784e5789de48778c915ae3b264240a3d3e59
uoon <u1001001n@gmail.com>
2025-12-16 18:10:29 +0900

    let

---
+++ ff/js/ch18/condition_exp.js
@@ -0,0 +1,8 @@
+/* condition_exp.js
+조건식(삼항식)
+식 ? true일 때 : false일 때
+*/
+
+let val = 10;
+const result = val % 2 == 0 ? '짝수' : '홀수';
+console.log('result:', result);
---
+++ ff/js/ch18/mission04.js
@@ -0,0 +1,3 @@
+let val = -20;
+const result = val < 0 ? -val : val;
+console.log('result:', result);
