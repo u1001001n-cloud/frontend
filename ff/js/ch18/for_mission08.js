3ea63b89906eb68ce200384e75ddacfc726f6995
uoon <u1001001n@gmail.com>
2025-12-18 17:19:09 +0900

    jsjs

--- ff/js/ch18/for_mission08.js
+++ ff/js/ch18/for_mission08.js
@@ -3,14 +3,29 @@
 const star = parseInt(Math.random() * 5.0) + 3; //3~7사이 랜덤값
 console.log("star:", star);
 
-for (let i = 1; i < star; i++) {
-  console.log("*");
+for (let i = 0; i < star; i++) {
+  let str = "";
+  for (let k = 0; k < star; k++) {
+    str += "*";
+  }
+  console.log(str);
 }
 
-let str = "";
+console.log("------------------");
 
+let str = "";
 for (let i = 0; i < star; i++) {
   str += "*";
 }
+for (let i = 0; i < star; i++) {
+  console.log(str);
+}
 
-console.log(str);
+console.log("------------------");
+let str2 = "";
+for (let i = 1; i <= star * star; i++) {
+  str2 += "*";
+  if (i % star === 0) {
+    str2 += "\n";
+  }
+}
