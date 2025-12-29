61c58864a3afabefbdfecef783794d8a082fdf64
KimWooJun <wojun74@naver.com>
2025-12-29 09:35:48 +0900

    gbgbjsjs

---
+++ ff/js/promise/jagong.js
@@ -0,0 +1,36 @@
+const nums1 = [1, 2, 3, 4, 5];
+
+let sum = 0;
+for(let i=0; i<nums1.length; i++){
+  if(sum[i] % 2 === 0){
+    console.log(sum[i]);
+  }
+}
+
+console.log("---------------------");
+
+const nums2 = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < nums2.length; i++) {
+  if (nums2[i] % 2 === 0) {
+    console.log(nums2[i]);
+  }
+}
+
+console.log("---------------------");
+
+const nums = [5, 8, 12, 7, 20];
+for(let i=0; i<nums.length; i++){
+  if(nums[i]>10){
+    console.log(nums[i]);
+  }
+}
+
+console.log("---------------------");
+
+const arr = [9, 14, 18, 56, 98];
+for(let i=0; i<arr.length; i++){
+  if(arr[i]>20){
+    console.log(arr[i]);
+  }
+}
